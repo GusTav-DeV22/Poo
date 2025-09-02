cf39acc595a9cee40ca955b83b57948a059330e8
GusTav-Dev22 <gustavo.r05@aluno.ifsc.edu.br>
2025-09-01 21:33:31 -0300

    Minha segunda caneta

---
+++ prolo/Aula/Caneta/app/src/main/java/org/example/Caneta.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Caneta {
+
+    private String cor;
+    private double nivelDeTinta;
+    private boolean tampada;
+
+
+
+}
---
+++ prolo/Aula/Caneta/app/src/main/java/org/example/Mouse.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Mouse {
+
+    private String cor;
+    private int peso;
+    private int quantidadeBotoes;
+    private boolean ligado;
+
+    public void clicar;
+    public void mover;
+    public void conectar;
+
+
+
+
+}
